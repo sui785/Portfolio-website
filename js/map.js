83dc698fa57441db10616b14e29e92798fc1f4c6
sui785 <devious_lowest1g@icloud.com>
2026-01-07 19:33:32 +0100

    Datacenter fix
    Adress changed

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-let map = L.map("dataMap").setView([51.2635, 4.4146], 16);
+let map = L.map("dataMap").setView([51.229528, 4.416761], 17);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
@@ -9,8 +9,8 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 // rechthoek rond de datacenter
 
 let bounds = [
-  [51.263238, 4.414454],
-  [51.263034, 4.414918],
+  [51.2294, 4.4165],
+  [51.2297, 4.417],
 ];
 
 // kleur van de rechthoek (#e60005)
@@ -18,9 +18,11 @@ let bounds = [
 L.rectangle(bounds, { color: "#e60005", weight: 1 }).addTo(map);
 
 // plaats van de marker
-let dataMarker = L.marker([51.263108, 4.41467]).addTo(map);
+let dataMarker = L.marker([51.22954, 4.41675]).addTo(map);
 
 // tekst bij de marker
 dataMarker
-  .bindPopup("<b>Datacenter United</b><br>Haifastraat 6<br>2030 Antwerpen")
+  .bindPopup(
+    "<b>Datacenter Antwerpen</b><br>Ellermanstraat 28<br>2030 Antwerpen"
+  )
   .openPopup();
